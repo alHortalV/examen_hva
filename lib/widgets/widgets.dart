4a314b4b22ee45604059858e1d3af268a952bc9b
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-12-13 09:56:47 +0100

    Clase de exportación de widgets

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'package:fl_examen_moviles_ahv/widgets/custom_text_form_field.dart';
 export 'package:fl_examen_moviles_ahv/widgets/custom_listview.dart';
 export 'package:fl_examen_moviles_ahv/widgets/custom_pista.dart';
+export 'package:fl_examen_moviles_ahv/widgets/custom_monitores.dart';
