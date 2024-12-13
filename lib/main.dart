7891c4d17edc9139acd2f26b1377cf1497fc0a54
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-12-13 09:28:02 +0100

    Creación del main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
+import 'package:fl_examen_moviles_ahv/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
+//home_screen_hva.dart
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      // home: const Listview2Screen(),
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.getAppRoutes(),
     );
   }
 }
