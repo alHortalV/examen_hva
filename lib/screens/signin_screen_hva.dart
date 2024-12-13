2d25b6e816e03150235c8ffff59a8fd6fc3d54f9
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-12-13 09:57:34 +0100

    Creación de la clase SignInScreen para realizar el inicio de sesión por medio de un formulario

---
+++ lib/screens/signin_screen_hva.dart
@@ -0,0 +1,87 @@
+import 'package:fl_examen_moviles_ahv/screens/screens.dart';
+import 'package:fl_examen_moviles_ahv/widgets/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    final Map<String, String> formValues = {};
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Center(
+                  child: FlutterLogo(
+                    size: 200,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                // Campo para usuario
+                CustomTextFormField(
+                  labelText: 'Usuario',
+                  suffixIcon: Icons.group_outlined,
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  capitalization: TextCapitalization.none,
+                  formProperty: 'usuario',
+                  formValues: formValues,
+                  validator: (value) {
+                    if (value == null || value.isEmpty || value.length < 6) {
+                      return 'Mínimo 6 caracteres';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                // Campo para contraseña
+                CustomTextFormField(
+                  labelText: 'Contraseña',
+                  obscureText: true,
+                  capitalization: TextCapitalization.none,
+                  formProperty: 'contraseña',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 24),
+                // Botón para el inicio de sesión
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Valida el formulario antes de hacer el print
+                      print('Usuario: ${formValues['usuario']}');
+                      print('Contraseña: ${formValues['contraseña']}');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ListViewScreen()),
+                      );
+                    } else {
+                      print('Por favor, completa los campos correctamente');
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 40),
+                  ),
+                  child: const Text('Sign In'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
