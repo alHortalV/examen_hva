2ddf9d39ecb97cc7404664144a66bc52e67bba4c
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-12-13 10:00:18 +0100

    Creación de la clase de exportación de los screens

---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:fl_examen_moviles_ahv/screens/home_screen_hva.dart';
+export 'package:fl_examen_moviles_ahv/screens/signin_screen_hva.dart';
+export 'package:fl_examen_moviles_ahv/screens/listview_screen_hva.dart';
+export 'package:fl_examen_moviles_ahv/screens/monitores_screen_hva.dart';
+export 'package:fl_examen_moviles_ahv/screens/pistas_screen_hva.dart';
